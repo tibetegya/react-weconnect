64281b15fa9a557943420cbe3266e63c536300a1
George Tibetegya <gtibetegya@gmail.com>
2018-07-12 19:08:54 +0300

    Update UserProfile.test.js

--- src/__tests__/auth/UserProfile.test.js
+++ src/__tests__/auth/UserProfile.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import UserProfile from '../../components/auth/userProfile'
+import UserProfile from '../../components/auth/UserProfile'
 import {MemoryRouter} from 'react-router-dom'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter';
