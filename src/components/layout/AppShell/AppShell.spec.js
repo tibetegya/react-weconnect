7d5912672b7039598a03ebe3b7c31c457f8a15df
George Tibetegya <gtibetegya@gmail.com>
2020-06-12 01:17:16 +0300

    setup jest and react testing library

---
+++ src/components/layout/AppShell/AppShell.spec.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'test-utils'
+import AppShell from '../AppShell'
+
+describe('AppShell Component tests', () => {
+  it('should render corectly', () => {
+    render(<AppShell />)
+  })
+})
