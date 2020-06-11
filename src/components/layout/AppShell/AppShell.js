aff235e8ffb6bcbea91d391c297f15366da26fda
George Tibetegya <gtibetegya@gmail.com>
2020-06-11 23:21:01 +0300

    nextjs setup

---
+++ src/components/layout/AppShell/AppShell.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AppShell = props => {
+  return (
+    <div>
+      AppShell
+    </div>
+  )
+}
+
+AppShell.propTypes = {
+
+}
+
+export default AppShell
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App' ;
-
-import './css/bootstrap.css';
-import './css/style.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
